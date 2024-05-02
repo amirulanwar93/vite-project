fb91e65e7ff1d5d84ccdc576e558298acbffd44b
Amirul Anwar <anwar_solong@yahoo.com.my>
2024-05-03 00:22:35 +0800

    first push

---
+++ src/random.js
@@ -0,0 +1,11 @@
+export const stailNama = {
+  color: "red",
+  fontSize: "1.5rem",
+  fontWeight: "bold",
+  padding: "1rem",
+  margin: "1rem",
+  backgroundColor: "yellow",
+  border: "1px solid black",
+  borderRadius: "1rem",
+  boxShadow: "1px 1px 1px 1px black",
+};
